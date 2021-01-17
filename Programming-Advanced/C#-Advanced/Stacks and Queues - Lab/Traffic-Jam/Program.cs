4f1b7d93cb288be2b19a1c4f99b92218ab1b62d1
Reaper041 <dimitar041@abv.bg>
2021-01-18 01:07:01 +0200

    Added project in Programming Advanced

---
+++ Programming-Advanced/C#-Advanced/Stacks and Queues - Lab/Traffic-Jam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPasses = int.Parse(Console.ReadLine());
+
+            Queue<string> cars = new Queue<string>();
+            string input = Console.ReadLine();
+            int count = 0;
+
+            while (input != "end")
+            {
+                if (input != "green")
+                {
+                    cars.Enqueue(input);
+                }
+                else
+                {
+                    for (int i = 0; i < numOfPasses; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            count++;
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
