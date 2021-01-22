966e9ef96e22c0656bc075718e340f5c83df3efa
Reaper041 <dimitar041@abv.bg>
2021-01-22 15:24:54 +0200

    Added exercises in Programming-Advanced

--- Programming-Advanced/C#-Advanced/Multidimensional Arrays - Lab/Jagged-Array-Modification/Program.cs
+++ Programming-Advanced/C#-Advanced/Multidimensional Arrays - Lab/Jagged-Array-Modification/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Jagged_Array_Modification
 {
@@ -6,7 +8,59 @@ namespace Jagged_Array_Modification
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+
+            int[,] matrix = new int[n, n];
+            int sum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = elements[col];
+                    sum += elements[col];
+                }
+            }
+
+            string[] input = Console.ReadLine().Split();
+
+            while (input[0] != "END")
+            {
+                string command = input[0];
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+                int value = int.Parse(input[3]);
+
+                if (row >= 0 && row < n && col >= 0 && col < n)
+                {
+                    if (command == "Add")
+                    {
+                        matrix[row, col] += value;
+                    }
+                    else if (command == "Subtract")
+                    {
+                        matrix[row, col] -= value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+
+                input = Console.ReadLine().Split();
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Programming-Advanced/C#-Advanced/Multidimensional Arrays - Lab/Pascal-Triangle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long[][] matrix = new long[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                matrix[row] = new long[row + 1];
+                for (int col = 0; col < row + 1; col++)
+                {
+                    if (row - 1 >= 0 && col < matrix[row].Length - 1)
+                    {
+                        matrix[row][col] += matrix[row - 1][col];
+                    }
+
+                    if (col - 1 >= 0 && row - 1 >= 0)
+                    {
+                        matrix[row][col] += matrix[row - 1][col - 1];
+                    }
+
+                    if (row == 0)
+                    {
+                        matrix[0][0] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(string.Join(' ', matrix[i]));
+            }
+        }
+    }
+}
