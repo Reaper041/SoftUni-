d2325422d5181ae98324bb55e9f27adaf6420bca
Reaper041 <dimitar041@abv.bg>
2021-01-27 23:19:27 +0200

    Added exercises in Programming-Advanced (Sets and Dictionaries Advanced)

---
+++ Programming-Advanced/C#-Advanced/Sets and Dictionaries Advanced - Lab/Cities-by-Continent-and-Country/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            Dictionary<string, Dictionary<string, List<string>>> continents =
+                new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = input[0];
+                string county = input[1];
+                string city = input[2];
+
+                if (continents.ContainsKey(continent))
+                {
+                    if (continents[continent].ContainsKey(county))
+                    {
+                        continents[continent][county].Add(city);
+                    }
+                    else
+                    {
+                        continents[continent].Add(county, new List<string>());
+                        continents[continent][county].Add(city);
+                    }
+                }
+                else
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                    continents[continent].Add(county, new List<string>());
+                    continents[continent][county].Add(city);
+                }
+            }
+
+
+            foreach (var continent in continents)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach (var country in continent.Value)
+                {
+                    Console.Write($"{country.Key} -> ");
+                    Console.WriteLine(string.Join(", ", country.Value));
+                }
+            }
+        }
+    }
+}
---
+++ Programming-Advanced/C#-Advanced/Sets and Dictionaries Advanced - Lab/Parking-Lot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carNumbers = new HashSet<string>();
+
+            string[] input = Console.ReadLine().Split(", ");
+
+            while (input[0] != "END")
+            {
+                string command = input[0];
+                string number = input[1];
+
+                if (command == "IN")
+                {
+                    carNumbers.Add(number);
+                }
+                else
+                {
+                    carNumbers.Remove(number);
+                }
+
+                input = Console.ReadLine().Split(", ");
+            }
+
+
+            if (carNumbers.Count > 0)
+            {
+                foreach (var carNumber in carNumbers)
+                {
+                    Console.WriteLine(carNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
