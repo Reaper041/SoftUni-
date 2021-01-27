2b11cbee4257d8c4c6194abf2f109db7c880bb09
Reaper041 <dimitar041@abv.bg>
2021-01-27 16:53:16 +0200

    Added exercises in Programming-Advanced (Sets and Dictionaries Advanced)

---
+++ Programming-Advanced/C#-Advanced/Sets and Dictionaries Advanced - Lab/Count-Same-Values-in-Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> numbers = new Dictionary<double, int>();
+
+            foreach (var number in input)
+            {
+                if (numbers.ContainsKey(number))
+                {
+                    numbers[number]++;
+                }
+                else
+                {
+                    numbers.Add(number, 1);
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
---
+++ Programming-Advanced/C#-Advanced/Sets and Dictionaries Advanced - Lab/Product-Shop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, Dictionary<string, double>> shops =
+                new Dictionary<string, Dictionary<string, double>>();
+
+            while (input[0] != "Revision")
+            {
+                string shop = input[0];
+                string product = input[1];
+                double price = double.Parse(input[2]);
+
+                if (shops.ContainsKey(shop))
+                {
+                    shops[shop].Add(product, price);
+                }
+                else
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                    shops[shop].Add(product, price);
+                }
+
+
+                input = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+
+            foreach (var shop in shops
+                .OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
