48ff1e9c5c895e96297a97d56a676dec26542f99
Reaper041 <dimitar041@abv.bg>
2021-01-29 16:01:38 +0200

    Added exercises in Programming-Advanced (Streams, Files and Directories)

---
+++ Programming-Advanced/C#-Advanced/Streams, Files and Directories - Exercise/Even-Lines/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"..\..\..\some-text.txt"))
+            {
+                string line = reader.ReadLine();
+                int counter = 0;
+
+                while (line != null)
+                {
+                    if (counter++ % 2 == 0)
+                    {
+                        List<char> splitters = new List<char>() { '-', ',', '.', '!', '?' };
+
+                        foreach (var splitter in splitters)
+                        {
+                            if (line.Contains(splitter))
+                            {
+                                line = line.Replace(splitter, '@');
+                            }
+                        }
+
+
+                        string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+
+                        Console.WriteLine(string.Join(' ', words.Reverse()));
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
