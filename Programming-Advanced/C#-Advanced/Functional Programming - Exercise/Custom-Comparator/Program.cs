806cfcd08b213151c19a11250ad0244a1531c595
Reaper041 <dimitar041@abv.bg>
2021-02-06 15:42:40 +0200

    Added exercises in Programming-Advanced (Functional Programming)

---
+++ Programming-Advanced/C#-Advanced/Functional Programming - Exercise/Custom-Comparator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Func<List<int>, List<int>> customComparator =
+                numbers =>
+                {
+                    List<int> evenList = new List<int>();
+                    List<int> oddList = new List<int>();
+
+                    foreach (var number in numbers)
+                    {
+                        if (number % 2 == 0) 
+                        {
+                            evenList.Add(number);
+                        }
+                        else
+                        {
+                            oddList.Add(number);
+                        }
+                    }
+
+                    numbers.Clear();
+
+                    numbers = numbers.Concat(evenList.OrderBy(n => n)).ToList();
+                    numbers = numbers.Concat(oddList.OrderBy(n => n)).ToList();
+
+
+                    return numbers;
+                };
+
+            nums = customComparator(nums);
+
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
---
+++ Programming-Advanced/C#-Advanced/Functional Programming - Exercise/List-Of-Predicates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> numbers = Enumerable.Range(1, n).ToList();
+
+            Func<int, bool> isSpecial =
+                number =>
+                {
+                    for (int i = 0; i < dividers.Count; i++)
+                    {
+                        if (number % dividers[i] != 0)
+                        {
+                            return false;
+                        }
+                    }
+                    
+                    return true;
+                };
+
+            List<int> special = numbers.Where(isSpecial).ToList();
+
+
+            Console.WriteLine(string.Join(' ', special));
+        }
+    }
+}
