0bddce448d26780db5c6741478ec62c6ac9d78bd
Reaper041 <dimitar041@abv.bg>
2021-02-14 22:53:55 +0200

    Added Programming-Advanced exam

---
+++ Programming-Advanced/C#-Advanced/C-sharp Advanced Retake Exam - 16 December 2020/Cooking/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liquidsArr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] ingredientsArr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> liquids = new Queue<int>(liquidsArr);
+            Stack<int> ingredients = new Stack<int>(ingredientsArr);
+
+
+            Dictionary<string, int> foodCooked = new Dictionary<string, int>()
+            {
+                {"Bread", 0},
+                {"Cake", 0},
+                {"Fruit Pie", 0},
+                {"Pastry", 0}
+            };
+
+            while (liquids.Count != 0 && ingredients.Count != 0)
+            {
+                int liquidValue = liquids.Dequeue();
+                int ingredientValue = ingredients.Pop();
+                int sum = liquidValue + ingredientValue;
+
+                switch (sum)
+                {
+                    case 25:
+                        foodCooked["Bread"]++;
+                        break;
+                    case 50:
+                        foodCooked["Cake"]++;
+                        break;
+                    case 75:
+                        foodCooked["Pastry"]++;
+                        break;
+                    case 100:
+                        foodCooked["Fruit Pie"]++;
+                        break;
+                    default:
+                        ingredients.Push(ingredientValue + 3);
+                        break;
+                }
+            }
+
+            if (foodCooked.All(x => x.Value > 0))
+            {
+                Console.WriteLine($"Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (liquids.Count == 0)
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+
+            if (ingredients.Count == 0)
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Ingredients left: {string.Join(", ", ingredients)}");
+            }
+
+            foreach (var food in foodCooked)
+            {
+                Console.WriteLine($"{food.Key}: {food.Value}");
+            }
+        }
+    }
+}
---
+++ Programming-Advanced/C#-Advanced/C-sharp Advanced Retake Exam - 16 December 2020/Selling/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+namespace Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] bakery = new char[n, n];
+            int myRow = 0;
+            int myCol = 0;
+            int firstORow = 0;
+            int firstOCol = 0;
+            int secondORow = 0;
+            int secondOCol = 0;
+            int sum = 0;
+
+            int count = 0;
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    bakery[row, col] = input[col];
+                    if (input[col] == 'S')
+                    {
+                        myRow = row;
+                        myCol = col;
+                    }
+
+                    if (input[col] == 'O' && count == 0)
+                    {
+                        firstORow = row;
+                        firstOCol = col;
+                        count++;
+                    }
+
+                    if (input[col] == 'O' && count == 1)
+                    {
+                        secondORow = row;
+                        secondOCol = col;
+                    }
+                }
+            }
+
+            bool flag = false;
+            while (sum < 50)
+            {
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "up":
+                        if (myRow - 1 < 0)
+                        {
+                            flag = true;
+                            bakery[myRow, myCol] = '-';
+                            break;
+                        }
+                        else if (char.IsNumber(bakery[myRow - 1, myCol]))
+                        {
+                            sum += int.Parse(bakery[myRow - 1, myCol].ToString());
+                            bakery[myRow - 1, myCol] = 'S';
+                            bakery[myRow, myCol] = '-';
+                            myRow -= 1;
+                        }
+                        else if (bakery[myRow - 1, myCol] == 'O')
+                        {
+                            bakery[myRow - 1, myCol] = '-';
+                            if (myRow - 1 != firstORow && myCol != firstOCol)
+                            {
+                                myRow = firstORow;
+                                myCol = firstOCol;
+                            }
+                            else
+                            {
+                                myRow = secondORow;
+                                myCol = secondOCol;
+                            }
+
+                            bakery[myRow, myCol] = 'S';
+                        }
+                        else
+                        {
+                            bakery[myRow - 1, myCol] = 'S';
+                            myRow -= 1;
+                        }
+                        break;
+                    case "down":
+                        if (myRow + 1 > n)
+                        {
+                            flag = true;
+                            break;
+                        }
+                        else if (char.IsNumber(bakery[myRow + 1, myCol]))
+                        {
+                            sum += int.Parse(bakery[myRow + 1, myCol].ToString());
+                            bakery[myRow + 1, myCol] = 'S';
+                            bakery[myRow, myCol] = '-';
+                            myRow += 1;
+                        }
+                        else if (bakery[myRow + 1, myCol] == 'O')
+                        {
+                            bakery[myRow + 1, myCol] = '-';
+                            if (myRow + 1 != firstORow && myCol != firstOCol)
+                            {
+                                myRow = firstORow;
+                                myCol = firstOCol;
+                            }
+                            else
+                            {
+                                myRow = secondORow;
+                                myCol = secondOCol;
+                            }
+
+                            bakery[myRow, myCol] = 'S';
+                        }
+                        else
+                        {
+                            bakery[myRow + 1, myCol] = 'S';
+                            myRow += 1;
+                        }
+                        break;
+                    case "left":
+                        if (myCol - 1 < 0)
+                        {
+                            flag = true;
+                            break;
+                        }
+                        else if (char.IsNumber(bakery[myRow, myCol - 1]))
+                        {
+                            sum += int.Parse(bakery[myRow, myCol - 1].ToString());
+                            bakery[myRow, myCol - 1] = 'S';
+                            bakery[myRow, myCol] = '-';
+                            myCol -= 1;
+                        }
+                        else if (bakery[myRow, myCol - 1] == 'O')
+                        {
+                            bakery[myRow, myCol - 1] = '-';
+                            if (myRow != firstORow && myCol - 1 != firstOCol)
+                            {
+                                myRow = firstORow;
+                                myCol = firstOCol;
+                            }
+                            else
+                            {
+                                myRow = secondORow;
+                                myCol = secondOCol;
+                            }
+
+                            bakery[myRow, myCol] = 'S';
+                        }
+                        else
+                        {
+                            bakery[myRow, myCol - 1] = 'S';
+                            myCol -= 1;
+                        }
+                        break;
+                    case "right":
+                        if (myCol + 1 > n)
+                        {
+                            flag = true;
+                            break;
+                        }
+                        else if (char.IsNumber(bakery[myRow, myCol + 1]))
+                        {
+                            sum += int.Parse(bakery[myRow, myCol + 1].ToString());
+                            bakery[myRow, myCol + 1] = 'S';
+                            bakery[myRow, myCol] = '-';
+                            myCol += 1;
+                        }
+                        else if (bakery[myRow, myCol + 1] == 'O')
+                        {
+                            bakery[myRow, myCol + 1] = '-';
+                            if (myRow != firstORow && myCol + 1 != firstOCol)
+                            {
+                                myRow = firstORow;
+                                myCol = firstOCol;
+                            }
+                            else
+                            {
+                                myRow = secondORow;
+                                myCol = secondOCol;
+                            }
+
+                            bakery[myRow, myCol] = 'S';
+                        }
+                        else
+                        {
+                            bakery[myRow, myCol + 1] = 'S';
+                            myCol += 1;
+                        }
+                        break;
+                }
+            }
+
+
+
+
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(bakery[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
