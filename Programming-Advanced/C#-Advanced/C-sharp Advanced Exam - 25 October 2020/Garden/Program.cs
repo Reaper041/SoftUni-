367010640513afd7befdfe2e45046567ea56f636
Reaper041 <dimitar041@abv.bg>
2021-02-15 16:09:06 +0200

    Added Programming Advanced exam

---
+++ Programming-Advanced/C#-Advanced/C-sharp Advanced Exam - 25 October 2020/Garden/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = dimentions[0];
+            int m = dimentions[1];
+
+            int[,] garden = CreateMatrix(n, m);
+
+            string[] input = Console.ReadLine().Split();
+
+            while (input[0] != "Bloom")
+            {
+                int myRow = int.Parse(input[0]);
+                int myCol = int.Parse(input[1]);
+
+                if (myRow < 0 || myRow >= n || myCol < 0 || myCol >= m) 
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    input = Console.ReadLine().Split();
+                    continue;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    garden[myRow, i]++;
+                }
+
+                for (int i = 0; i < m; i++)
+                {
+                    garden[i, myCol]++;
+                }
+
+                garden[myRow, myCol]--;
+
+                input = Console.ReadLine().Split();
+            }
+
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    Console.Write(garden[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static int[,] CreateMatrix(int rows, int cols)
+        {
+
+            int[,] garden = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    garden[row, col] = 0;
+                }
+            }
+
+            return garden;
+        }
+    }
+}
---
+++ Programming-Advanced/C#-Advanced/C-sharp Advanced Exam - 25 October 2020/Scheduling/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tasksArr = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] threadsArr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> tasks = new Stack<int>(tasksArr);
+
+            Queue<int> threads = new Queue<int>(threadsArr);
+
+            int taskToBeKilled = int.Parse(Console.ReadLine());
+
+            int taskValue = 0;
+            int threadValue = 0;
+
+            while (tasks.Contains(taskToBeKilled))
+            {
+                taskValue = tasks.Pop();
+                threadValue = threads.Peek();
+
+                if (taskValue == taskToBeKilled)
+                {
+                    continue;
+                }
+
+                threads.Dequeue();
+                if (threadValue < taskValue)
+                {
+                    tasks.Push(taskValue);
+                }
+            }
+
+
+            Console.WriteLine($"Thread with value {threadValue} killed task {taskToBeKilled}");
+
+            Console.WriteLine(string.Join(' ', threads));
+        }
+    }
+}
