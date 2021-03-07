6c7e3d96915deb4a7e408b910d68dc8dfd444d3c
Reaper041 <dimitar041@abv.bg>
2021-03-07 23:05:41 +0200

    Added exercises in C#-OPP (Inheritance)

---
+++ Programming-Advanced/C#-OOP/Inheritance - Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMilliliters = 50;
+        private const decimal CoffeePrice = 3.50m;
+
+
+        public Coffee(string name, decimal price, double milliliters, double caffeine)
+            : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public virtual double Caffeine { get; set; }
+    }
+}
