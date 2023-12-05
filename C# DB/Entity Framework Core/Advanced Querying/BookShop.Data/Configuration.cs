f14254fe254b538ada66a70243be3b63b688dd5a
dimitar041 <041Mitko041>
2023-12-05 19:07:22 +0200

    Added Exercises

---
+++ C# DB/Entity Framework Core/Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString 
+            => "Server=DESKTOP-5P50UCA;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ C# DB/Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetGoldenBooks(db));
+
+            string asd = "13.43m";
+
+            decimal ass = decimal.Parse(asd);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            AgeRestriction ageRestriction;
+
+            bool parseSuccess = Enum.TryParse<AgeRestriction>(command, true, out ageRestriction);
+
+            if (!parseSuccess)
+            {
+                return String.Empty;
+            }
+
+            List<string> books = context
+                .Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToList();
+
+            return String.Join(Environment.NewLine, books);
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var bookTitles = context
+                .Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .ToList();
+
+            return String.Join(Environment.NewLine, bookTitles);
+        }
+    }
+}
