4a14813c844b11460dea24a54b7bf46c6f9e8b88
dimitar041 <041Mitko041>
2024-08-05 16:52:56 +0300

    Added Steams Exercises

---
+++ Programming-Advanced/C#-Advanced/Streams, Files and Directories - Exercise/Line-Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using (StreamReader streamReader = new StreamReader("../../../text.txt") )
+{
+    string line = streamReader.ReadLine()!;
+    int counter = 1;
+
+    using (StreamWriter streamWriter = new StreamWriter("../../../result.txt"))
+    {
+        List<char> punctuationMarks = new List<char>()
+            {
+                '-', ',', '.', '!', '?', '\''
+            };
+        while (line != null)
+        {
+            int punctuationMarksCount = 0;
+            
+
+            foreach (char c in punctuationMarks)
+            {
+                if (line.Contains(c))
+                {
+                    punctuationMarksCount++;
+                }
+            }
+            
+
+            streamWriter.WriteLine($"Line {counter}: {line} ({line.Length})({punctuationMarksCount})");
+
+            line = streamReader.ReadLine()!;
+            counter++;
+        }
+    }
+}
---
+++ Programming-Advanced/C#-Advanced/Streams, Files and Directories - Exercise/Word-Count/Program.cs
@@ -0,0 +1,36 @@
+﻿Dictionary<string, int> result = new Dictionary<string, int>();
+
+using (StreamReader streamReader = new StreamReader("../../../words.txt"))
+{
+    string word = streamReader.ReadLine()!;
+
+    while (word != null)
+    {
+        int wordTimesCount = 0;
+
+        using (StreamReader streamReaderText = new StreamReader("../../../text.txt"))
+        {
+            string[] textChunks = streamReaderText.ReadToEnd().Split(" ").ToArray();
+
+            foreach (string chunk in textChunks)
+            {
+                if (chunk.Contains(word))
+                {
+                    wordTimesCount++;
+                }
+            }
+        }
+
+        result[word] = wordTimesCount;
+
+        word = streamReader.ReadLine()!;
+    }
+}
+
+using (StreamWriter writer = new StreamWriter("../../../actualResult.txt", true))
+{
+    foreach (var pair in result.OrderByDescending(w => w.Value))
+    {
+        writer.WriteLine($"{pair.Key} - {pair.Value}");
+    }
+}
